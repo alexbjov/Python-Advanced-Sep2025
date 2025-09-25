c92e3af393756a32be052a5893f90832a0912675
alexbj <ajoveski@gmail.com>
2025-09-25 04:52:32 +0200

    Complete starred exercise of chapter 4

--- 04-multidimensional-lists/bombs.py
+++ 04-multidimensional-lists/bombs.py
@@ -4,26 +4,29 @@ matrix = []
 for _ in range(rows):
 	row_data = [int(x) for x in input().split()]
 	matrix.append(row_data)
-	# print(row_data)
 
 cmds = input().split()
 coordinates = []
 for cmd in cmds:
-	b_x, b_y = map(int, cmd.split(','))
+	b_x, b_y = list(map(int, cmd.split(',')))
 	coordinates.append((b_x, b_y))
-# print(coordinates)
+
+possible_moves = ((-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1),
+				  (1, 1), (0, 0))
 
 for bomb_x, bomb_y in coordinates:
-	# print(bomb_x, bomb_y)
-	start_x = max(bomb_x - 1, 0)
-	end_x = min(bomb_x + 2, rows)
-	start_y = max(bomb_y - 1, 0)
-	end_y = min(bomb_y + 2, rows)
-	bomb_power = matrix[bomb_x][bomb_y]
-	for i in range(start_x, end_x):
-		for j in range(start_y, end_y):
-			if matrix[i][j] > 0:
-				matrix[i][j] -= bomb_power
+	if matrix[bomb_x][bomb_y] <= 0:
+		continue
+	
+	for move in possible_moves:
+		next_row = bomb_x + move[0]
+		next_col = bomb_y + move[1]
+		if not 0 <= next_row < rows or not 0 <= next_col < rows:
+			continue
+		
+		bomb_power = matrix[bomb_x][bomb_y]
+		if matrix[next_row][next_col] > 0:
+			matrix[next_row][next_col] -= bomb_power
 
 counter_alive_cells = 0
 sum_alive_cells = 0
