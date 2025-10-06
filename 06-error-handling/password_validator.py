2a626e02aaaa61f7a9157c692432b1f0a6934b9f
alexbj <ajoveski@gmail.com>
2025-10-06 10:01:55 +0200

    Improve code structure for validation

--- 06-error-handling/password_validator.py
+++ 06-error-handling/password_validator.py
@@ -1,40 +1,47 @@
+from string import punctuation
+
+
 class PasswordTooShortError(Exception):
 	pass
+
 
 class PasswordTooCommonError(Exception):
 	pass
 
+
 class PasswordNoSpecialCharactersError(Exception):
 	pass
+
 
 class PasswordContainsSpacesError(Exception):
 	pass
 
+
 PASS_MIN_LENGTH = 8
-special_chars = {'@', '*', '&', '%'}
 
 password = input()
 while password != 'Done':
-
 	if len(password) < PASS_MIN_LENGTH:
-		raise PasswordTooShortError('Password must contain at least 8 characters')
-
-	if password.isdigit() or password.isalpha():
-		raise PasswordTooCommonError('Password must be a combination of digits, letters, and special characters')
-
+		raise PasswordTooShortError(
+			'Password must contain at least 8 characters')
+	
 	counter_special_chars = 0
-	for special_char in special_chars:
+	for special_char in punctuation:
 		counter_special_chars += password.count(special_char)
-
-	if counter_special_chars == 0:
-		raise PasswordNoSpecialCharactersError ('Password must contain at least 1 special character')
-
-	elif counter_special_chars == len(password):
-		raise PasswordTooCommonError('Password must be a combination of digits, letters, and special characters')
-
+	
+	if (password.isdigit() or password.isalpha() or (
+			counter_special_chars == len(password) and len(password) > 0)):
+		raise PasswordTooCommonError(
+			'Password must be a combination of digits, letters, and special characters')
+	
+	if counter_special_chars == 0 and len(password) > 0:
+		raise PasswordNoSpecialCharactersError(
+			'Password must contain at least 1 special character')
+	
 	if ' ' in password:
-		raise PasswordContainsSpacesError('Password must not contain empty spaces')
-
+		raise PasswordContainsSpacesError(
+			'Password must not contain empty spaces')
+	
 	print('Password is valid')
-
+	
 	password = input()
